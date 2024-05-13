d7e09806f81f191a73a750a1e402bc0f1e995e77
Alper Yildirim <alperyldrm96@gmail.com>
2024-05-13 13:20:27 +0300

    auth ve redirect middlewareler ayarlandi. Custom dashboard yapildi.

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,7 @@ exports.loginUser = async (req, res) => {
       if (same) {
         // USER SESSION
         req.session.userID = user._id;
-        res.status(200).redirect('/');
+        res.status(200).redirect('/users/dashboard');
       } else {
         res.status(401).send('Password is incorrect');
       }
@@ -43,5 +43,13 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
   });
 };
--- routers/userRoute.js
+++ routers/userRoute.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const authController = require('../controllers/authController');
 
+const authMiddleware = require('../middlewares/authMiddleware');
+
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser); // = localhost:3000/users/signup
 router.route('/login').post(authController.loginUser); // = localhost:3000/users/login
-router.route('/logout').get(authController.logoutUser); // 
+router.route('/logout').get(authController.logoutUser); //
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage); // = localhost:3000/users/dashboard
 module.exports = router;
